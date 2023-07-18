d4696bec7a5a5e154ed29d6a00136f8a9ad0141e
Ben Mahoney <benmahoney10@gmail.com>
2023-07-18 14:57:06 +1200

    Testing stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,5 @@
-<h1>Hello welcome to by website</h1>
+<input type="text" /> <!-- this is a text input -->
+<input type="text" value="I am an input"/> <!-- this is a text input with a value -->
+<br/> <!-- this is a break tag - creates a line break between things -->
+<hr/> <!-- this is a horizontal rule -->
+<img src="https://bit.ly/30x9TsC" width="300" /> <!-- this is an image tag -->
