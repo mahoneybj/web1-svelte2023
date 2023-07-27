de5f5113bb92ef4cd17f198e904881672f1eda34
Benjamin Mahoney (1000107339) <MAHOBJ1@student.op.ac.nz>
2023-07-28 08:52:19 +1200

    Homework 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
-<input type="text" /> <!-- this is a text input -->
-<input type="text" value="I am an input"/> <!-- this is a text input with a value -->
-<br/> <!-- this is a break tag - creates a line break between things -->
-<hr/> <!-- this is a horizontal rule -->
-<img src="https://bit.ly/30x9TsC" width="300" /> <!-- this is an image tag -->
+<h2>HTML practice</h2>
+<div></div>
+<ul>
+  <li>cat</li>
+  <li>dog</li>
+  <li>fish</li>
+  <li>bird</li>
+</ul>
+<div><img src="https://www.bu.edu/files/2018/06/t-rex-DestinationDino1.jpg" width="500" /></div>
+<a href="https://op.ac.nz">Otago Polytechnic</a>
