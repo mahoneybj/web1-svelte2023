43b4cbb93de4e74aaad3038ea6ae88d5f756b2e7
Ben Mahoney <benmahoney10@gmail.com>
2023-08-25 09:22:20 +1200

    Minor div changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <h1>DOGS</h1>
-<div></div>
+<div class = "test">
 <ul>
   <li>Cool</li>
   <li>Dog</li>
 </ul>
-
+</div>
 <div>
   <img src="https://hips.hearstapps.com/hmg-prod/images/golden-retriever-royalty-free-image-506756303-1560962726.jpg?crop=1.00xw:0.756xh;0,0.0756xh&resize=1200:*" width="500" />
 </div>
