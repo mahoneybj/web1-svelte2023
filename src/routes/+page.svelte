5bb71d4c87d28bb11b07e0a3bbe7563d2d5b55e7
Ben Mahoney <benmahoney10@gmail.com>
2023-09-05 17:32:13 +1200

    Added background image, playing around

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<div class="background">
+  <h2>Dog</h2>
+</div>
+
 <h1>DOGS</h1>
 <div class = "test">
 <ul>
@@ -5,8 +9,6 @@
   <li>Dog</li>
 </ul>
 </div>
-<div>
-  <img src="https://hips.hearstapps.com/hmg-prod/images/golden-retriever-royalty-free-image-506756303-1560962726.jpg?crop=1.00xw:0.756xh;0,0.0756xh&resize=1200:*" width="500" />
-</div>
+
 
 
