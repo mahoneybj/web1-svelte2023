111dfef552c08f069a04658cf240064075aa7344
Ben Mahoney <benmahoney10@gmail.com>
2023-10-30 22:30:38 +1300

    Removed ul list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,11 @@
+<div class="Outer-box">
 <div class="background">
   <h2>Dog</h2>
 </div>
 
 
-<div class = "test">
-  <h1>DOGS</h1>
-<ul>
-  <li>Cool</li>
-  <li>Dog</li>
-  <li>Cool</li>
-  <li>Dog</li>
-  <li>Cool</li>
-  <li>Dog</li>
-  <li>Cool</li>
-  <li>Dog</li>
-  <li>Cool</li>
-  <li>Dog</li>
-  <li>Cool</li>
-  <li>Dog</li>
-  <li>Cool</li>
-  <li>Dog</li>
-  <li>Cool</li>
-  <li>Dog</li>
-  <li>Cool</li>
-  <li>Dog</li>
-  <li>Cool</li>
-  <li>Dog</li>
-  <li>Cool</li>
-  <li>Dog</li>
-</ul>
-</div>
 
+</div>
+</div>
 
 
