0391bf9d801969583c79a63ed51db0e4f9908e21
Ben Mahoney <benmahoney10@gmail.com>
2023-10-30 23:35:27 +1300

    Added dog cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,22 @@
 <div class="background">
   <h2>Dog</h2>
 </div>
+<div class="card-row">
+  <div class="card">
+    <img src="https://a.pinatafarm.com/671x680/c3c5aee140/homophobic-dog.jpg" alt="Avatar" style="width:100%">
+    <div class="container">
+      <h4><b>Judging sausage</b></h4>
+      <p>Strong gaydar</p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="https://miro.medium.com/v2/resize:fit:1400/1*rIkmavUeqyRySwlQdA9kKg.jpeg" alt="Avatar" style="width:100%">
+    <div class="container">
+      <h4><b>Walter</b></h4>
+      <p>MEOW</p>
+    </div>
+  </div>
+</div>
 
 </div>
 
