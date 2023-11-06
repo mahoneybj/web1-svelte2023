debc71fff4489a0f9924cb5318f50e0c9af64f0d
mahoneybj <benmahoney10@gmail.com>
2023-11-07 01:22:35 +1300

    Working on fetching dogs to fill cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,15 @@
+<script>
+  import { } from '$lib/index.js';
+</script>
+
 <div class="Outer-box">
 <div class="background">
   <h2>Dog</h2>
+</div>
+<div class="random-button-area">
+  <div class="random-button">
+      <button class="random-dog" on:click={() => randomDog()}>Random Dog</button>
+  </div>
 </div>
 <div class="card-row">
   <div class="card">
