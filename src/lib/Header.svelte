c8260c1dd335de4043e47a48c912c701cb3a55bb
Ben Mahoney <benmahoney10@gmail.com>
2023-11-10 10:32:08 +1300

    Removed unused css

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,5 +2,4 @@
   <!-- Header of nav buttons that take you to Home and Random Dog -->
   <a href="/" class="button">Home</a>
   <a href="random-dog" class="button">Random Dog</a>
-  <a href="about" class="button">About</a>
 </header>
