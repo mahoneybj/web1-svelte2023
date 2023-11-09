6cdf0527b3819f0ee6ce065fce7216de0139b0c3
Ben Mahoney <benmahoney10@gmail.com>
2023-11-10 09:55:49 +1300

    Added comments to Header.svelte

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,5 +1,6 @@
 <header>
-    <a href="/" class="button">Home</a>
+    <!-- Header of nav buttons that take you to Home and Random Dog -->
+    <a href="/" class="button">Home</a> 
     <a href="random-dog" class="button">Random Dog</a>
     <a href="about" class="button">About</a>
 </header>
