8ffc7a24545c3b40e5835c44360ee4d6a74a7fdc
mahoneybj <benmahoney10@gmail.com>
2023-11-09 22:17:28 +1300

    Cleaning up random test code

--- src/routes/random-dog/+page.svelte
+++ src/routes/random-dog/+page.svelte
@@ -1,9 +1,6 @@
 <script>
     import { dogArray } from '$lib/index.js';
 
-
-    let dogs = null;
-
     function randomDog(){
         const randomIndex = Math.floor(Math.random() * $dogArray.dogBreeds.length);
         const rand = $dogArray.dogBreeds[randomIndex];
