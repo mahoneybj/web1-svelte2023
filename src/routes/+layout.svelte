7fd5958307415e661f22c31c19438e0ed4ccd446
Ben Mahoney <benmahoney10@gmail.com>
2023-11-10 10:03:55 +1300

    Added comments to layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,17 @@
 <script>
     import Header from "$lib/Header.svelte";
     import Footer from "$lib/Footer.svelte";
-  </script>
+</script>
 
 <svelte:head>
 	<link rel="stylesheet" href="css/main.css" />
-  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"> <!-- Links googles Roboto font to stylesheet -->
 </svelte:head>
 
-<Header/>
+<Header/> <!-- Header at top of page -->
 
 <main>
   <slot />
 </main>
 
-<Footer/>
+<Footer/> <!-- Footer at bottom of page -->
